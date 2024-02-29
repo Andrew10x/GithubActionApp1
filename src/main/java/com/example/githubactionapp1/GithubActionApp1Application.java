1012595033ee0aa03dcddeea9bfb0fdb93c8a3a8
Andrew <svt.andrey1631@gmail.com>
2024-02-29 13:58:29 +0200

    first commit

---
+++ src/main/java/com/example/githubactionapp1/GithubActionApp1Application.java
@@ -0,0 +1,19 @@
+package com.example.githubactionapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubActionApp1Application {
+    @GetMapping("/")
+    public String get() {
+        return "Welcome to the masquerade";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionApp1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubactionapp1/GithubActionApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionApp1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
